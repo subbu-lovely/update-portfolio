54dbf0f3c804275d23ce16de03b610a22de555ea
unknown <subbukondrupathi@gmail.com>
2026-02-28 17:07:19 +0530

    Initial commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,11 +7,11 @@ const settings = {
 
 //SEO Related settings
 const seo = {
-  title: "Ashutosh's Portfolio",
+  title: "Subbu Kondruparthi 's Portfolio",
   description:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   og: {
-    title: "Ashutosh Hathidara Portfolio",
+    title: "Subbu Kondruparthi  Portfolio",
     type: "website",
     url: "http://ashutoshhathidara.com/",
   },
@@ -19,9 +19,9 @@ const seo = {
 
 //Home Page
 const greeting = {
-  title: "Ashutosh Hathidara",
-  logo_name: "AshutoshHathidara",
-  nickname: "layman_brother",
+  title: "Subrahmanym ",
+  logo_name: "Subrahmanyam ",
+  nickname: "subbu",
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
